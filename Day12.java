a2a9c7110808680bcfa9c148bb0296f9d689509b
ANUSHKASONI2099 <153723294+ANUSHKASONI2099@users.noreply.github.com>
2025-09-30 10:47:20 +0200

    commit

--- Day12.java
+++ Day12.java
@@ -1,3 +1,19 @@
+// Print Square...
+
+import java.util.Scanner;
+
 public class Day12 {
-    
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+            System.out.print("*");
+            }
+           System.out.println(" ");
+        }
+  
+    }
+   
 }
