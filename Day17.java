7e23d837163b35e40eba4e0d0a6decbebd04a8a3
ANUSHKASONI2099 <153723294+ANUSHKASONI2099@users.noreply.github.com>
2025-10-02 15:15:47 +0200

    c

--- Day17.java
+++ Day17.java
@@ -1,3 +1,17 @@
+// Maxium product of 2 elements problem 
+
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Day17 {
-    
+    public static int maxProduct(int[] nums){
+            Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+        Arrays.sort(nums);
+        int product1 = nums[n-2] * nums[n-1];
+        int product2 = nums[0] * nums[1];
+
+        return Math.max(product1, product2);
+
+    }
 }
