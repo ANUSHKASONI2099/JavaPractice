421e7c68ef5b1973de3388679281ed450c9c2a43
ANUSHKASONI2099 <153723294+ANUSHKASONI2099@users.noreply.github.com>
2025-10-02 15:12:13 +0200

    Update Day16.java

--- Day16.java
+++ Day16.java
@@ -8,4 +8,14 @@ public class Day16 {
 
 
     }
+
+    public void reverse(char arr , int left , int right){
+        int left = 0 , right = arr.length - 1;
+        while(left <= right){
+            swap(arr,left,right);
+
+        }
+        right--;
+        left++;
+    }
 }
