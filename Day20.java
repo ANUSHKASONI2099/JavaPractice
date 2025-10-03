bd8e4130b02e1dd72bb961d68bca7f98c8084407
ANUSHKASONI2099 <153723294+ANUSHKASONI2099@users.noreply.github.com>
2025-10-03 11:24:38 +0200

    Update Day20.java

--- Day20.java
+++ Day20.java
@@ -1,3 +1,16 @@
+// Suffix Sum..........
+
 public class Day20 {
-    
+    public int[] suffixSum(int[] nums){
+        int n = nums.length;
+        int[] suffix = new int[n];
+
+        suffix[n-1] = nums[n-1];
+
+        for(int i = n-2; i>= 0 ;  i--){
+            suffix[i] = nums[i] + suffix[i+1];
+        }
+        return suffix;
+        
+    }
 }
