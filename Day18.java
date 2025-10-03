7331afff753801ec6140eb6c548b105aecd5db9e
ANUSHKASONI2099 <153723294+ANUSHKASONI2099@users.noreply.github.com>
2025-10-03 11:20:31 +0200

    c

--- Day18.java
+++ Day18.java
@@ -1,3 +1,20 @@
+// Leetcode 1588 ....... Sum of all odd length Subarray
+
 public class Day18 {
-    
+    public int sum(int[] arr) {
+        int ans = 0;
+        for (int left = 0; left < arr.length; left++) {
+            int sum = 0;
+
+            for (int right = left; right < arr.length; right++) {
+                sum += arr[right];
+            }
+            if ((right - left + 1) % 2 == 1) {
+                ans += sum;
+            }
+
+        }
+
+        return ans;
+    }
 }
