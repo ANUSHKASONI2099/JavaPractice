66459b1bd916c4d7262cb8b358ab86e39a10a598
ANUSHKASONI2099 <153723294+ANUSHKASONI2099@users.noreply.github.com>
2025-10-03 11:28:47 +0200

    n

--- Day21.java
+++ Day21.java
@@ -1,3 +1,13 @@
+// Finding Largets Altitude........... 1732 Leetcode
+
 public class Day21 {
-    
+   public int altitude(int[] gain){
+    int ans = 0 ;
+    int altitude = 0 ;
+    for(int val : gain){
+        altitude += val;
+        ans = Math.max(ans , altitude);
+    }
+    return ans;
+   } 
 }
